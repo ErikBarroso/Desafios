5808e0a7b8681eb9258a67a698ee1b71b8e09627
Erik Barroso <coderik1996@gmail.com>
2023-02-20 07:36:08 -0300

    multiplos de 7

---
+++ Ex 04 multiplo de 7/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int A ;
+  cin >> A  ;  
+
+  if (A % 7 == 0) {
+    cout << "É MULTIPLO DE 7" << endl;
+  } else {
+    cout << "ESSE NÃO É" << endl;
+  }
+  return 0;
+}
