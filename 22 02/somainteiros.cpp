cbd68c901dccc1310b11f8e7831cda4d3ad8e631
Erik Barroso <coderik1996@gmail.com>
2023-02-23 06:54:26 -0300

    treino

--- 22 02/somainteiros.cpp
+++ 22 02/somainteiros.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
- int n, x, resultado=0;
+ int n = 50, x = 5000, resultado=0;
   cin >> n;  
-  for (int i = 0; i < n; i++){
+  for (int i = 0; i <= n; i++){
         cin >> x;
-        resultado = resultado + x;       
+        resultado += x;       
 }
-   cout<< resultado << endl;
+   cout<< "O Resultado é:"<< resultado << endl;
     system("pause");
     return 0;
 }
