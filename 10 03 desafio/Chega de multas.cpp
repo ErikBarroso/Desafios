247e03b76b94f4ea3e89a64ec9d85fcc1057f18b
Erik Barroso <coderik1996@gmail.com>
2023-03-10 18:10:22 -0300

    desafio

---
+++ 10 03 desafio/Chega de multas.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>;
+using namespace std;
+
+int main()
+{
+    double a;    
+    cin >> a;
+
+    cout << fixed << setprecision (2);
+    cout << a / 0.62137;
+    system("pause");
+    return 0;
+}
