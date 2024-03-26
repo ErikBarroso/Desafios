8885f00135032b459ac75851b0c0c121897fd542
Erik Barroso <coderik1996@gmail.com>
2024-03-26 09:19:34 -0300

    save

---
+++ Sequência de Fibonacci/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int primeiro = 0, segundo = 1, proximo;
+
+for(int i=0 ;proximo =100; i++) {
+  cout << proximo << endl;le
+
+  proximo = primeiro + segundo;
+  primeiro = segundo;
+  segundo = proximo;
+}
+
+  return 0;
+}
+
+// Escreva uma função para gerar os primeiros '100' números da sequência de Fibonacci
