53453758d6fe37e3a0f2fb40070dfef6bd83ef1e
Erik Barroso <coderik1996@gmail.com>
2024-03-27 13:31:14 -0300

    sequencia de fibonacci correta

--- Sequência de Fibonacci/main.cpp
+++ Sequência de Fibonacci/main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-int primeiro = 0, segundo = 1, proximo = 0;
+int main()
+{
+  int primeiro =0 , segundo = 1 , proximo = 1;
 
-for(int i = 0 ; i<100; i++) {
-  cout << proximo << endl;
-  proximo = primeiro + segundo;
-  primeiro = segundo;
-  segundo = proximo;
+  for(int i =0; i <100; i++) {
+    cout << proximo << endl;
+    proximo = segundo + primeiro;
+    primeiro = segundo;
+    segundo = proximo;
+  }
+
+  return 0 ;
 }
-
-return 0;
-}
-
-// Escreva uma função para gerar os primeiros '100' números da sequência de Fibonacci
-// somar cada numero com o numero que antecede
