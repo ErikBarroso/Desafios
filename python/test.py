e4517772fb830f88f2bde2febb6ffab9dd6503b3
ErikBarroso <coderik1996@gmail.com>
2025-03-25 20:26:05 -0300

    aprendendo python

---
+++ python/test.py
@@ -0,0 +1,15 @@
+age = 3
+teste = f'3 + 4 = {3+4}'
+
+name = 'Erik'
+age = 28
+# print(f'{name=}, {age=}')
+
+door_lookd = True
+# if door_lookd:
+#     print('Aberta')
+# else:
+    # print('Fechada')
+
+result= True + 2
+# print(result)
